89d296961a8acf0ff86ca922484f316a6f5b567b
Saif Siddique <saifsiddiqueshikrani@gmail.com>
2025-12-17 03:52:32 +0500

    I think that I am there otherwise Allah di marzi

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { initializeSocket } = require('./socket/socketHandler');
 
 dotenv.config();
 
-const instance = await mongoose.connect(process.env.MONGO_URI)
+const instance = await mongoose.connect(`${process.env.MONGO_URI}/hostler-database`)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('MongoDB Connection Error:', err));
 
